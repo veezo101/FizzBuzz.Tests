4abbee88a13365a88ff08f09ec33cacd5eb6582c
Aviral Singh <aviral@intelpeek.com>
2023-06-05 13:58:31 +0400

    switch ver

--- FizzBuzz/FizzBuzzGame.cs
+++ FizzBuzz/FizzBuzzGame.cs
@@ -4,19 +4,32 @@
 // "Buzz" if number is divisible by 5.
 // echo number (as a string) if none of the above conditions are true.
 
+using System.Linq;
+using System.Xml.Serialization;
+
 namespace FizzBuzz.Tests
 {
     public class FizzBuzzGame
     {
         public static string Play(int number)
         {
-            if (number % 3 == 0 && number % 5 == 0)
-                return "FizzBuzz";
-            if (number % 3 == 0)
-                return "Fizz";
-            if (number % 5 == 0)
-                return "Buzz";
-            return number.ToString();
+            string res = "";
+            switch (number)
+            {
+                case var n when n % 3 == 0 && n % 5 == 0:
+                    res = res + "FizzBuzz";
+                    break;
+                case var n when n%3 == 0:
+                    res = res + "Fizz";
+                    break;
+                case var n when n % 5 == 0:
+                    res = res + "Buzz";
+                    break;
+                default:
+                    res = res + number.ToString();
+                    break;
+            }
+            return res;
         }
     }
 }
