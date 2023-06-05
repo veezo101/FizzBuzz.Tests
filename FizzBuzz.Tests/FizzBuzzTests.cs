208206e67f2f1c28341733ef166cfbb162d83579
Aviral Singh <aviral@intelpeek.com>
2023-06-05 12:00:26 +0400

    Add project files.

---
+++ FizzBuzz.Tests/FizzBuzzTests.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+
+//? Given an integer, return a string where:
+// "Fizzbuzz" if number is divisible by 3 and 5.
+// "Fizz" if number is divisible by 3.
+// "Buzz" if number is divisible by 5.
+// echo number (as a string) if none of the above conditions are true.
+
+namespace FizzBuzz.Tests
+{
+    public class FizzBuzzTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
